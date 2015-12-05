318425badf40332248ac25019648ff97ff6f9a17
dogmai <matthewlvh1@me.com>
2015-12-05 12:09:48 -0500

    Initial Commit

---
+++ GitExample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExample2
+//
+//  Created by Dogmai on 12/5/15.
+//  Copyright © 2015 2ndLawApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
